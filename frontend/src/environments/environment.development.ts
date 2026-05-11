21cb982c3b6c6b3fe30667273fb23269fffd024c
Teilor Souza Barcelos <tsb-@live.com>
2026-05-10 22:14:39 -0300

    fix: Environment unuseless files removed

--- frontend/src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-import { getApiUrl } from '../app/core/utils/config';
-
-export const environment = {
-  production: false,
-  apiUrl: getApiUrl()
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-import { getApiUrl } from '../app/core/utils/config';
-
-export const environment = {
-  production: true,
-  apiUrl: getApiUrl()
-};
